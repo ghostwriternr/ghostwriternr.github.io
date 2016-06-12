0b2abde0dd3832960a78789913fa59cbf3e61dfa
Naresh <ghostwriternr@gmail.com>
2016-06-12 23:19:02 +0530

    Minor cosmetic adjustments

---
+++ js/style.js
@@ -0,0 +1,3 @@
+var parent = document.getElementById('mainContentParent');
+var child = document.getElementById('mainContent');
+child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
