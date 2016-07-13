cddb5401c734d68490482a8f0f0bb404ad88e3e3
Naresh <ghostwriternr@gmail.com>
2016-07-13 08:43:39 +0000

    HTML Boilerplate <3

--- js/style.js
+++
@@ -1,3 +0,0 @@
-var parent = document.getElementById('mainContentParent');
-var child = document.getElementById('mainContent');
-child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
